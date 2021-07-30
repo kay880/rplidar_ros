5a3862e91d280a823e36bfeb9003109966438504
kay2018 <643805493@qq.com>
2021-07-30 09:56:37 +0800

    Add files via upload

--- sdk/src/arch/win32/net_serial.h
+++ sdk/src/arch/win32/net_serial.h
@@ -34,14 +34,53 @@
 
 #pragma once
 
-#include "hal/types.h"
+#include "hal/abs_rxtx.h"
 
-#define delay(x)   ::Sleep(x)
+namespace rp{ namespace arch{ namespace net{
 
-namespace rp{ namespace arch{
-    void HPtimer_reset();
-    _u32 getHDTimer();
-}}
+class raw_serial : public rp::hal::serial_rxtx
+{
+public:
+    enum{
+        SERIAL_RX_BUFFER_SIZE = 512,
+        SERIAL_TX_BUFFER_SIZE = 128,
+        SERIAL_RX_TIMEOUT     = 2000,
+        SERIAL_TX_TIMEOUT     = 2000,
+    };
 
-#define getms()   rp::arch::getHDTimer()
+    raw_serial();
+    virtual ~raw_serial();
+    virtual bool bind(const char * portname, _u32 baudrate, _u32 flags = 0);
+    virtual bool open();
+    virtual void close();
+    virtual void flush( _u32 flags);
+    
+    virtual int waitfordata(size_t data_count,_u32 timeout = -1, size_t * returned_size = NULL);
 
+    virtual int senddata(const unsigned char * data, size_t size);
+    virtual int recvdata(unsigned char * data, size_t size);
+
+    virtual int waitforsent(_u32 timeout = -1, size_t * returned_size = NULL);
+    virtual int waitforrecv(_u32 timeout = -1, size_t * returned_size = NULL);
+
+    virtual size_t rxqueue_count();
+
+    virtual void setDTR();
+    virtual void clearDTR();
+
+protected:
+    bool open(const char * portname, _u32 baudrate, _u32 flags);
+    void _init();
+
+    char _portName[20];
+    uint32_t _baudrate;
+    uint32_t _flags;
+
+    OVERLAPPED _ro, _wo;
+    OVERLAPPED _wait_o;
+    volatile HANDLE _serial_handle;
+    DCB _dcb;
+    COMMTIMEOUTS _co;
+};
+
+}}}
--- sdk/src/arch/win32/timer.cpp
+++ sdk/src/arch/win32/timer.cpp
@@ -32,16 +32,31 @@
  *
  */
 
-#pragma once
-
-#include "hal/types.h"
-
-#define delay(x)   ::Sleep(x)
+#include "sdkcommon.h"
+#include <mmsystem.h>
+#pragma comment(lib, "Winmm.lib")
 
 namespace rp{ namespace arch{
-    void HPtimer_reset();
-    _u32 getHDTimer();
+
+static LARGE_INTEGER _current_freq;
+
+void HPtimer_reset()
+{
+    BOOL ans=QueryPerformanceFrequency(&_current_freq);
+    _current_freq.QuadPart/=1000;
+}
+
+_u32 getHDTimer()
+{
+    LARGE_INTEGER current;
+    QueryPerformanceCounter(&current);
+
+    return (_u32)(current.QuadPart/_current_freq.QuadPart);
+}
+
+BEGIN_STATIC_CODE(timer_cailb)
+{
+    HPtimer_reset();
+}END_STATIC_CODE(timer_cailb)
+
 }}
-
-#define getms()   rp::arch::getHDTimer()
-
